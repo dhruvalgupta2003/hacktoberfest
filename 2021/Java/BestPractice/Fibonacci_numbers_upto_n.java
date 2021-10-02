68ff89bbb46559270e2fe777fc3a8f9ef9efc4a8
dhruvalgupta2003 <72140427+dhruvalgupta2003@users.noreply.github.com>
2021-10-03 00:13:42 +0530

    Added A fibonacci sequence upto n finder

---
+++ 2021/Java/BestPractice/Fibonacci_numbers_upto_n.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+
+public class fibonacci_numbers {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int a=0;
+        int b=1;
+        int c=1;
+        while (a<=n){
+            System.out.println(a);
+            a=b;
+            b=c;
+            c=a+b;
+        }
+
+    }
+}
